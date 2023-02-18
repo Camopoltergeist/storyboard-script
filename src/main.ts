a6685e8be53cdb03000b3d238e39c454b7b64bf7
Camopoltergeist <camopoltergeist@outlook.com>
2023-02-18 04:20:42 +0200

    Setup project

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import { Scene, OrthographicCamera, WebGLRenderer } from "three";
+
+const renderer = new WebGLRenderer({
+	alpha: true,
+	antialias: true,
+	powerPreference: "high-performance"
+});
+
+const scene = new Scene();
+const camera = new OrthographicCamera(0, 1920, 0, 1080, 0, 100);
+
+let resizeObserver = new ResizeObserver((entries, observer) => {
+	for(const entry of entries){
+		const width = entry.devicePixelContentBoxSize[0].inlineSize;
+		const height = entry.devicePixelContentBoxSize[0].blockSize;
+
+		renderer.setSize(width, height, false);
+
+		camera.right = width;
+		camera.bottom = height;
+	}
+});
+
+resizeObserver.observe(renderer.domElement, { box: "device-pixel-content-box" });
+
+document.body.appendChild(renderer.domElement);
