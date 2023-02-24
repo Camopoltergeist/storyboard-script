7ef364d02c4358c751f3b36a5711a1cc9f002a4b
Camopoltergeist <camopoltergeist@outlook.com>
2023-02-24 17:56:03 +0200

    Created helper function to create lines.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Scene, OrthographicCamera, WebGLRenderer, Color } from "three";
+import { Scene, OrthographicCamera, WebGLRenderer, Color, Mesh, BoxGeometry, LineBasicMaterial, MeshBasicMaterial, Line, PerspectiveCamera, LineSegments, BufferGeometry, Vector3 } from "three";
+import { degToRad } from "three/src/math/MathUtils";
 
 const renderer = new WebGLRenderer({
 	alpha: true,
@@ -7,7 +8,9 @@ const renderer = new WebGLRenderer({
 });
 
 const scene = new Scene();
-const camera = new OrthographicCamera(0, 1920, 0, 1080, 0, 100);
+const camera = new PerspectiveCamera(57, 16 / 9, 0.01, 1000);
+
+camera.translateZ(10);
 
 let resizeObserver = new ResizeObserver((entries, observer) => {
 	for(const entry of entries){
@@ -16,8 +19,7 @@ let resizeObserver = new ResizeObserver((entries, observer) => {
 
 		renderer.setSize(width, height, false);
 
-		camera.right = width;
-		camera.bottom = height;
+		camera.aspect = width / height;
 	}
 });
 
@@ -28,5 +30,49 @@ document.body.appendChild(renderer.domElement);
 function step(time: DOMHighResTimeStamp){
 	renderer.render(scene, camera);
 }
+
+function getBoxLines(): BufferGeometry{
+	const geometry = new BufferGeometry();
+
+	const points = [
+		new Vector3(-0.5, -0.5, -0.5),
+		new Vector3(0.5, -0.5, -0.5),
+		new Vector3(0.5, 0.5, -0.5),
+		new Vector3(-0.5, 0.5, -0.5),
+
+		new Vector3(-0.5, -0.5, 0.5),
+		new Vector3(0.5, -0.5, 0.5),
+		new Vector3(0.5, 0.5, 0.5),
+		new Vector3(-0.5, 0.5, 0.5),
+	];
+
+	const indices = [
+		0, 1,
+		1, 2,
+		2, 3,
+		3, 0,
+
+		4, 5,
+		5, 6,
+		6, 7,
+		7, 4,
+
+		0, 4,
+		1, 5,
+		2, 6,
+		3, 7
+	];
+
+	geometry.setFromPoints(points);
+	geometry.setIndex(indices);
+
+	return geometry;
+}
+
+const cube = new LineSegments(getBoxLines(), new LineBasicMaterial());
+cube.rotateX(degToRad(45));
+cube.rotateZ(degToRad(45));
+
+scene.add(cube);
 
 renderer.setAnimationLoop(step);
